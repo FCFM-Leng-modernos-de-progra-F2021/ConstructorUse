e7e69c8cb384cabc8f1537e3ee348fda04fd711a
Rolando <rolando.fcfm2020@gmail.com>
2021-03-24 19:46:26 -0600

    first commit

---
+++ ConstructorDemo/Numbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorDemo
+{
+    public class Numbers
+    {
+        public int Count { get; set; }
+        public List<int> MyNumbers { get; set; }
+        //Cuando no hay constructor, el constructor es "invisible" o lo que es lo mismo 
+        //como si estuviera escrito esto
+        //public Numbers()
+        //{
+
+        //}
+
+        public Numbers()
+        {
+            MyNumbers = new List<int>() { 1, 2, 5, 10 };
+            Count = MyNumbers.Count;
+        }
+    }
+}
---
+++ ConstructorDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConstructorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Notacion como la de java
+            //Numbers myNumbersObject = new Numbers();
+
+            var myNumbersObject = new Numbers();
+
+            Console.WriteLine("¿Cuantos numeros tengo? :" + myNumbersObject.Count);
+
+            Console.WriteLine("¿Cual es el primer elemento? : " + myNumbersObject.MyNumbers[0]);
+
+        }
+    }
+}
